e89ca7a1e2cf94d3028f131c0303ce15b924e707
P_Banchev <petkobanchev1122@gmail.com>
2026-06-22 15:10:09 +0200

    Initial db migration

---
+++ Migrations/20260622130043_Initial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HolidayAssessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Holidays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    LocalName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CountryCode = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Fixed = table.Column<bool>(type: "bit", nullable: false),
+                    Global = table.Column<bool>(type: "bit", nullable: false),
+                    Counties = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    LaunchYear = table.Column<int>(type: "int", nullable: true),
+                    Types = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Holidays", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Holidays_CountryCode_Date",
+                table: "Holidays",
+                columns: new[] { "CountryCode", "Date" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Holidays");
+        }
+    }
+}
