c166d0e5a9ceaa5a97191e1e64e20d22d4588189
P_Banchev <petkobanchev1122@gmail.com>
2026-06-22 14:41:38 +0200

    Added a model and a dbContext for the Holiday.
    Removed example template files

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using HolidayAssessment.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HolidayAssessment.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Holiday> Holidays => Set<Holiday>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Holiday>(entity =>
+            {
+                entity.Property(x => x.CountryCode)
+                    .HasMaxLength(2);
+
+                entity.Property(x => x.LocalName)
+                    .HasMaxLength(100);
+
+                entity.Property(x => x.Name)
+                    .HasMaxLength(200);
+
+                entity.Property(x => x.Types)
+                    .HasMaxLength(200);
+
+                entity.Property(x => x.Counties)
+                    .HasMaxLength(500);
+
+                entity.HasIndex(x => new { x.CountryCode, x.Date })
+                    .IsUnique();
+            });
+        }
+    }
+}
---
+++ Models/Holiday.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HolidayAssessment.Models
+{
+    public class Holiday
+    {
+        public int Id { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        [MaxLength(100)]
+        public string LocalName { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(2)]
+        public string CountryCode { get; set; } = string.Empty;
+
+        public bool Fixed { get; set; }
+
+        public bool Global { get; set; }
+
+        public string? Counties { get; set; }
+
+        public int? LaunchYear { get; set; }
+
+        public string Types { get; set; } = string.Empty;
+    }
+}
