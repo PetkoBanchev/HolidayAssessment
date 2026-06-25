eb900e12cbb5ec221143bcfa11140e6863f03cf8
P-Banchev <p.banchev@recreate365.com>
2026-06-25 16:52:34 +0200

    - Added a year validator - Added the min and max years supported by Nager in a separate static class. (There is no API endpoint for supported years. The supported years were discovered by doing trial and error country+year request until the supported year range was discovered)

---
+++ Constants/NagerConstraints.cs
@@ -0,0 +1,8 @@
+﻿namespace HolidayAssessment.Constants
+{
+    public static class NagerConstraints
+    {
+        public const int MinYear = 1976;
+        public const int MaxYear = 2076;
+    }
+}
---
+++ Validators/YearValidator.cs
@@ -0,0 +1,12 @@
+﻿using HolidayAssessment.Constants;
+
+namespace HolidayAssessment.Validators
+{
+    public static class YearValidator
+    {
+        public static bool Validate(int year)
+        {
+            return year < NagerConstraints.MinYear || year > NagerConstraints.MaxYear;
+        }
+    }
+}
