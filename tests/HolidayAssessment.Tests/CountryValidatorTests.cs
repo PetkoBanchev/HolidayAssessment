9339cf03529b3f30554e6c14acda370f10de5c57
P-Banchev <p.banchev@recreate365.com>
2026-06-27 18:17:21 +0200

    Added unit tests for the CountryValidator

---
+++ tests/HolidayAssessment.Tests/CountryValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿namespace HolidayAssessment.Tests
+{
+    using FluentAssertions;
+    using HolidayAssessment.Services;
+    using HolidayAssessment.Validators;
+    using Moq;
+    using Xunit;
+
+    public class CountryValidatorTests
+    {
+        [Theory]
+        [InlineData("NL")]
+        [InlineData("nl")]
+        [InlineData("Nl")]
+        [InlineData("nL")]
+        public async Task ValidateCountryCode_Should_NotThrow_WhenCodeIsValid(string input)
+        {
+            var mockCache = new Mock<ICountryCacheService>();
+
+            mockCache.Setup(x => x.GetCountryCodesAsync()).ReturnsAsync(new List<string> { "NL", "DE", "FR" });
+
+            var validator = new CountryValidator(mockCache.Object);
+
+            Func<Task> act = async () =>
+                await validator.ValidateCountryCodeAsync(input);
+
+            await act.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task ValidateCountryCode_ShouldThrow_WhenCodeIsInvalid()
+        {
+            var mockCache = new Mock<ICountryCacheService>();
+
+            mockCache.Setup(x => x.GetCountryCodesAsync()).ReturnsAsync(new List<string> { "NL", "DE", "FR" });
+
+            var validator = new CountryValidator(mockCache.Object);
+
+            Func<Task> act = async () =>
+                await validator.ValidateCountryCodeAsync("XX");
+
+            await act.Should().ThrowAsync<InvalidOperationException>().WithMessage("*Invalid country code*");
+        }
+    }
+}
