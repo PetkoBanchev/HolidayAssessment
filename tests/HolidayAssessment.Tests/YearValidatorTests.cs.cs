acc9660289a1add0072b30140f7da844783d112e
P-Banchev <p.banchev@recreate365.com>
2026-06-27 17:53:19 +0200

    Added a "Tests" project to the solution - Made a unit test for the Year validator

---
+++ tests/HolidayAssessment.Tests/YearValidatorTests.cs.cs
@@ -0,0 +1,37 @@
+﻿namespace HolidayAssessment.Tests
+{
+    using HolidayAssessment.Validators;
+    using Xunit;
+
+    public class YearValidatorTests
+    {
+        [Fact]
+        public void Validate_Should_NotThrow_WhenYearIsValid()
+        {
+            // Arrange
+            var year = 2000;
+
+            // Act
+            var action = () => YearValidator.Validate(year);
+
+            // Assert
+            Assert.Null(Record.Exception(action));
+        }
+
+        [Fact]
+        public void Validate_ShouldThrow_WhenYearIsTooSmall()
+        {
+            var year = 1975;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => YearValidator.Validate(year));
+        }
+
+        [Fact]
+        public void Validate_ShouldThrow_WhenYearIsTooLarge()
+        {
+            var year = 2077;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => YearValidator.Validate(year));
+        }
+    }
+}
